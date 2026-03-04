9c0b57040e9d536c95eff31c59cccc349884323b
msburberryy-web <ms.burberryy@gmail.com>
2026-03-04 03:46:21 +0000

    added github page configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Amore-Wedding-Simulator-v1.1/',
       server: {
         port: 3000,
         host: '0.0.0.0',
